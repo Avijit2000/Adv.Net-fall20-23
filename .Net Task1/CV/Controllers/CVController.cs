887217d7dccd3f07df24fdb86fd90c417c0a3bdf
Avijit Sarker Tonmoy <71463366+Avijit2000@users.noreply.github.com>
2022-09-26 22:38:45 +0600

    Files upload

---
+++ .Net Task1/CV/Controllers/CVController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CV.Controllers
+{
+    public class CVController : Controller
+    {
+        // GET: CV
+        public ActionResult Intro()
+        {
+            return View();
+        }
+      
+        public ActionResult Personaldetails()
+        {
+            ViewBag.Name = "Avijit Sarker Tonmoy";
+            ViewBag.PermanentAddress = "Dhaka, Bangladesh";
+
+            return View();
+        }
+
+        public ActionResult Education()
+        {
+            return View();
+        }
+        public ActionResult Experience()
+        {
+            return View();
+        }
+        public ActionResult Skill()
+        {
+            return View();
+        }
+    }
+}
